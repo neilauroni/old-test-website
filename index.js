a08d902d033a2f7ccfdd9e12f68ad96a0c78a8bf
neilauroni <68305760+neilauroni@users.noreply.github.com>
2020-08-15 16:47:15 -0400

    i dont even know

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function buttonClicked() {
     let firstName = document.getElementById("fname").value;
     let lastName = document.getElementById("lname").value;
     console.log(firstName, lastName)
-    document.getElementById('text').innerHTML = "shut up  " + firstName + ' ' + lastName
+    document.getElementById('text').innerHTML = "hi  " + firstName + ' ' + lastName
 }
+
 
